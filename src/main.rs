d5e904447b3b58f4562752813a567a65f293bfc0
George Gkioulis <ggkioulis@suse.com>
2023-11-26 17:09:25 +0100

    Implement rudamentary ECS

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,106 @@
-use rltk::{Rltk, GameState};
+use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
 
-struct State {}
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+#[derive(Component)]
+struct LeftMover {}
+
+struct State {
+    ecs: World
+}
+
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
+
+        self.run_systems();
+
         ctx.print(1, 1, "Hello Rust World");
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
+
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>,
+                        WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
+        for (_lefty,pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 { pos.x = 79; }
+        }
+    }
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker{};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
+
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
-    let gs = State{ };
+    let mut gs = State {
+        ecs: World::new()
+    };
+
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
+
+    // Add the Player Character
+    gs.ecs
+       .create_entity()
+       .with(Position { x: 40, y: 25 })
+       .with(Renderable {
+           glyph: rltk::to_cp437('@'),
+           fg: RGB::named(rltk::YELLOW),
+           bg: RGB::named(rltk::BLACK),
+       })
+       .build();
+
+
+    // Add the red smileys
+    for i in 0..10 {
+       gs.ecs
+           .create_entity()
+           .with(Position { x: i * 7, y: 20 })
+           .with(Renderable {
+               glyph: rltk::to_cp437('☺'),
+               fg: RGB::named(rltk::RED),
+               bg: RGB::named(rltk::BLACK),
+           })
+           .with(LeftMover {})
+           .build();
+    }
+
     rltk::main_loop(context, gs)
 }
