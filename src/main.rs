913e4b85b94cbe530fc0177d2fcbb6b4bb7b9cd8
Georgios Gkioulis <georgios.gkioulis@paessler.com>
2023-12-15 12:45:12 +0100

    Generate rooms randomly

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
+use rltk::{GameState, Rltk, RGB};
 use specs::prelude::*;
-use std::cmp::{max, min};
 use specs_derive::Component;
 
-#[derive(PartialEq, Copy, Clone)]
-enum TileType {
-    Wall, Floor
-}
+mod map;
+pub use map::*;
+mod player;
+pub use player::*;
+mod rect;
+pub use rect::Rect;
 
 #[derive(Component)]
 struct Position {
@@ -31,30 +32,6 @@ struct State {
     ecs: World
 }
 
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    // Player movement
-    match ctx.key {
-        None => {} // Nothing happened
-        Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-            _ => {}
-        },
-    }
-}
-
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
@@ -66,6 +43,9 @@ impl GameState for State {
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
+        let map = self.ecs.fetch::<Vec<TileType>>();
+
+        draw_map(&map, ctx);
 
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
@@ -89,8 +69,8 @@ impl<'a> System<'a> for LeftWalker {
 
 impl State {
     fn run_systems(&mut self) {
-        let mut lw = LeftWalker{};
-        lw.run_now(&self.ecs);
+        //let mut lw = LeftWalker{};
+        //lw.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -104,11 +84,12 @@ fn main() -> rltk::BError {
     let mut gs = State {
         ecs: World::new()
     };
-
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
+
+    gs.ecs.insert(new_map_rooms_and_corridors());
 
     // Add the Player Character
     gs.ecs
@@ -123,19 +104,19 @@ fn main() -> rltk::BError {
        .build();
 
 
-    // Add the red smileys
-    for i in 0..10 {
-       gs.ecs
-           .create_entity()
-           .with(Position { x: i * 7, y: 20 })
-           .with(Renderable {
-               glyph: rltk::to_cp437('☺'),
-               fg: RGB::named(rltk::RED),
-               bg: RGB::named(rltk::BLACK),
-           })
-           .with(LeftMover {})
-           .build();
-    }
+    // // Add the red smileys
+    // for i in 0..10 {
+    //    gs.ecs
+    //        .create_entity()
+    //        .with(Position { x: i * 7, y: 20 })
+    //        .with(Renderable {
+    //            glyph: rltk::to_cp437('☺'),
+    //            fg: RGB::named(rltk::RED),
+    //            bg: RGB::named(rltk::BLACK),
+    //        })
+    //        .with(LeftMover {})
+    //        .build();
+    // }
 
     rltk::main_loop(context, gs)
 }
